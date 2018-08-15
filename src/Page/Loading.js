ed34360cb1c8e8a62b69fd14909f3a10e34d091b
roland <rolandcanuto@outlook.com>
2018-08-15 08:49:08 -0700

    forgot to update something

--- src/Page/Loading.js
+++ src/Page/Loading.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export default class Loading extends Component {
   componentDidMount() {
-    window.location.href = 'http://localhost:3000/1/about';
+    window.location.href = '/1/about';
   }
 
   render() {
