71b3e8fe2030e2585d046b47447082ebc59c1fd9
Roland <rolandcanuto@outlook.com>
2018-10-03 00:45:42 -0700

    removed terminal errors

--- src/Page/About.js
+++ src/Page/About.js
@@ -6,7 +6,7 @@ const About = (props) => {
 				<div className="a-wrapper">
 				<div className='wasd'>
 					<div className='g-f a-aboutContainer'>
-						<p className='a-about'>{"Hello, I'm Roland Canuto a Software Engineer living in Tracy, California. As a kid, I've always liked to take apart my things to see how they work and sometimes successfully... putting them back together. Today, ever since the first output of 'Hello World', I've had a passion for programming. I love to create simple yet eye catching websites and reliable backend databases. "}</p>
+						<p className='a-about'>{"Hello, I'm Roland Canuto a Full Stack Engineer living in Tracy, California. As a kid, I've always liked to take apart my things to see how they work and sometimes successfully... putting them back together. Today, ever since the first output of 'Hello World', I've had a passion for programming. I love to create simple yet eye catching websites and reliable backend databases. "}</p>
 					</div>
 				</div>
 				</div>
