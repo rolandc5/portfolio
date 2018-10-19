8ef270d46edeea4a53c7fed1caea9c861cc28119
Roland <rolandcanuto@outlook.com>
2018-10-19 09:36:12 -0700

    shortened contacts info

--- src/Page/Desktop/Contact.js
+++ src/Page/Desktop/Contact.js
@@ -7,7 +7,7 @@ const Contact = (props) => {
 					<div className='g-f c-contactContainer'>
 						<div className='c-contact'>
 							<div><p className='g-f c-fontColor c-contactHeader'>Let's Connect</p></div>
-							<div><p className='g-f c-fontColor c-contactFont'>so I could join your team or just want to hire me for freelancing or just want to talk about cool tech stuff or anything you want to talk about.</p></div>
+							<div><p className='g-f c-fontColor c-contactFont'>so that I can join your team or just want to talk about cool tech stuff.</p></div>
 							<div className='c-contactButtonContainer'>
 								<a className='g-f c-contactButton'target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/roland-canuto-43437941/'>linkedIn</a>
 								<a className='g-f c-contactButton' href="mailto:rolandfcanuto@gmail.com">mail</a>
